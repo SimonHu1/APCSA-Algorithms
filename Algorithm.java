6fff4c5d0a8be25cc92da1629f6de04b55206ef4
Simon Hu <simonh35@nycstudents.net>
2023-11-09 12:38:42 -0500

    jhxcjhxkzxg

---
+++ Algorithm.java
@@ -0,0 +1,8 @@
+public class Algorithm
+{
+    public int numberLength(int number)
+    {
+        String num = number+"";
+        return num.length();
+    }
+}
---
+++ Main.java
@@ -0,0 +1,4 @@
+public class Main
+{
+    Algorithm algorithm = new Algorithm();
+}
