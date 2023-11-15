b71ebed5a8b92a99835c2d2f1aa43604a6718a43
Simon Hu <simonh35@nycstudents.net>
2023-11-15 14:30:10 -0500

    Just 4.3 algorithms

--- Main.java
+++ Main.java
@@ -2,28 +2,6 @@ public class Main
 {
     public static void main(String[]args)
     {
-//        System.out.println(Algorithm.isDivisible(9,3));
-//        System.out.println(Algorithm.isDivisible(100,100));
-//        System.out.println(Algorithm.isDivisible(1,2));
-//
-//        System.out.println(Algorithm.sumOfOddsBetween(4,9));
-//        System.out.println(Algorithm.sumOfOddsBetween(10,10));
-//        System.out.println(Algorithm.sumOfOddsBetween(99,101));
-//
-//        System.out.println(Algorithm.multiplesOfTenBetween(10,99));
-//        System.out.println(Algorithm.multiplesOfTenBetween(98,99));
-//        System.out.println(Algorithm.multiplesOfTenBetween(6,32));
-//
-//        System.out.println(Algorithm.sumOfDigits(321));
-//        System.out.println(Algorithm.sumOfDigits(10));
-//        System.out.println(Algorithm.sumOfDigits(101230));
-//
-//        System.out.println(Algorithm.biggestPow2Before(800));
-//        System.out.println(Algorithm.biggestPow2Before(2000));
-//        System.out.println(Algorithm.biggestPow2Before(16));
-
-        //4.3 || Developing Algorithms Using Strings
-
         System.out.println(Algorithm.stringContainsOther("Hello World", "World"));
         System.out.println(Algorithm.stringContainsOther("Hello","hello"));
         System.out.println(Algorithm.stringContainsOther("super","superpower"));
